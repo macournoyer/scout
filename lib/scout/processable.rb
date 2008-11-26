176ff4aacdb6d8acb21294c3bff931da5019763c
macournoyer <macournoyer@gmail.com>
2008-11-26 10:11:09 -0500

    Refactor some stuff in a module

---
+++ lib/scout/processable.rb
@@ -0,0 +1,30 @@
+module Scout
+  module Processable
+    def process
+      raise StandardError, "Oh plz plz, implement #{self.class}\#process"
+    end
+    
+    def process!
+      process
+    rescue Object
+      speak "Error processing #{self.class.name}: #{$!.message}"
+      puts "#{$!}\n\t" + $!.backtrace.join("\n\t")
+    end
+    
+    def room
+      @bot.room
+    end
+    
+    def speak(message)
+      @bot.room.speak message
+    end
+    
+    def paste(message)
+      @bot.room.paste message
+    end
+    
+    def run_command(trigger, *args)
+      Command.find(trigger).new(from, bot, trigger, args).process!
+    end
+  end
+end
---
+++ test/listener_test.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/test_helper'
+
+class ListenerTest < Test::Unit::TestCase
+  def setup
+    @room = Tinder::Room.new stub, 1, 'Test room'
+    @bot = Scout::Bot.new @room
+  end
+  
+  def test_notify_welcome
+    assert_kind_of Scout::Listeners::Welcome, Scout::Listener.notify({:person => 'marc', :message => 'Bob has entered the room'}, @bot).first
+  end
+end
