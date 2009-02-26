4ef925d2e105f357561be1bd0007bbd3ac36ee95
Thanh Vinh Tang <zelnox@gmail.com>
2009-02-26 14:08:30 -0500

    can extract github description. I iz rebel and didn’t write a test.

--- lib/scout/listeners/url_listener.rb
+++ lib/scout/listeners/url_listener.rb
@@ -10,10 +10,27 @@ module Scout
       reacts_to LINK_RE
       
       def process
-        if message[LINK_RE, 1].match(/twitter\.com.+status.+/i)
+        # if message[LINK_RE, 1].match(/twitter\.com.+status.+/i)
+        #   tweet_content = extract_tweet_content(message[LINK_RE, 1])
+        #   unless tweet_content.blank?
+        #     paste Text::Format.new(:text => tweet_content).paragraphs
+        #   end
+        # else
+        #   page_title = extract_page_title(message[LINK_RE, 1])
+        #   unless page_title.blank?
+        #     speak "Last link: #{page_title}"
+        #   end
+        # end
+        case message[LINK_RE, 1]
+        when /twitter\.com.+status.+/i
           tweet_content = extract_tweet_content(message[LINK_RE, 1])
           unless tweet_content.blank?
             paste Text::Format.new(:text => tweet_content).paragraphs
+          end
+        when %r%github\.com.+/tree/master%i
+          github_description = extract_github_description(message[LINK_RE, 1])
+          unless github_description.blank?
+            paste Text::Format.new(:text => github_description).paragraphs
           end
         else
           page_title = extract_page_title(message[LINK_RE, 1])
@@ -35,6 +52,10 @@ module Scout
       
       def extract_tweet_content(url)
         extract_html_content(url, 'span.entry-content')
+      end
+      
+      def extract_github_description(url)
+        extract_html_content(url, 'span#repository_description')
       end
       
       def extract_html_content(url, hpricot_matcher)
