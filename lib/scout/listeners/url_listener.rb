67068cdde51e72f7cf64caf7a6b22d638645570c
Thanh Vinh Tang <zelnox@gmail.com>
2009-02-26 19:12:09 -0500

    Clean up UrlListener

--- lib/scout/listeners/url_listener.rb
+++ lib/scout/listeners/url_listener.rb
@@ -10,24 +10,13 @@ module Scout
       reacts_to LINK_RE
       
       def process
-        # if message[LINK_RE, 1].match(/twitter\.com.+status.+/i)
-        #   tweet_content = extract_tweet_content(message[LINK_RE, 1])
-        #   unless tweet_content.blank?
-        #     paste Text::Format.new(:text => tweet_content).paragraphs
-        #   end
-        # else
-        #   page_title = extract_page_title(message[LINK_RE, 1])
-        #   unless page_title.blank?
-        #     speak "Last link: #{page_title}"
-        #   end
-        # end
         case message[LINK_RE, 1]
         when /twitter\.com.+status.+/i
           tweet_content = extract_tweet_content(message[LINK_RE, 1])
           unless tweet_content.blank?
             paste Text::Format.new(:text => tweet_content).paragraphs
           end
-        when %r%github\.com.+/tree/master%i
+        when %r(github\.com.+/tree/master)i
           github_description = extract_github_description(message[LINK_RE, 1])
           unless github_description.blank?
             paste Text::Format.new(:text => github_description).paragraphs
@@ -41,13 +30,7 @@ module Scout
       end
       
       def extract_page_title(url)
-        return nil unless url
-        Timeout.timeout(2) do
-          doc = Hpricot(open(url))
-          (doc/:title).inner_html
-        end
-      rescue Exception
-        nil
+        extract_html_content(url, ':title')
       end
       
       def extract_tweet_content(url)
--- test/listeners/url_listener_test.rb
+++ test/listeners/url_listener_test.rb
@@ -8,17 +8,17 @@ class UrlListenerTest < Test::Unit::TestCase
   
   def test_notify_with_url
     assert_kind_of Scout::Listeners::UrlListener, 
-                   Scout::Listener.notify( {:person => 'unicorn', :message => '<a href="http://kottke.org/">http://kottke.org/</a>'}, @bot).first
+                   Scout::Listener.notify( {:person => "unicorn", :message => "<a href=\\\"http://kottke.org/\\\">http://kottke.org/</a>"}, @bot).first
   end
   
   def test_notify_with_url_should_extract_page_title
     Scout::Listeners::UrlListener.any_instance.expects(:extract_page_title)
-    Scout::Listener.notify( {:person => 'unicorn', :message => '<a href="http://kottke.org/">http://kottke.org/</a>'}, @bot).first.process
+    Scout::Listener.notify( {:person => "unicorn", :message => "<a href=\\\"http://kottke.org/\\\">http://kottke.org/</a>"}, @bot).first.process
   end
 
   def test_notify_with_tweet_should_extract_content
     Scout::Listeners::UrlListener.any_instance.expects(:extract_tweet_content)
-    Scout::Listener.notify( {:person => 'unicorn', :message => '<a href="http://twitter.com/yukihiro_matz/status/1213839957">http://twitter.com/yukihiro_matz/status/1213839957</a>'}, @bot).first.process
+    Scout::Listener.notify( {:person => "unicorn", :message => "<a href=\\\"http://twitter.com/yukihiro_matz/status/1213839957\\\">http://twitter.com/yukihiro_matz/status/1213839957</a>"}, @bot).first.process
   end
   
 end
