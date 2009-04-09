731d5c3cec4b0b8deb1250980ffa02b599320d4a
Thanh Vinh Tang <zelnox@gmail.com>
2009-04-09 10:55:42 -0400

    Back to escaped backslash. Updated hpricot. Let’s see.

--- lib/scout/listeners/url_listener.rb
+++ lib/scout/listeners/url_listener.rb
@@ -6,7 +6,7 @@ require "text/format"
 module Scout
   module Listeners
     class UrlListener < Scout::Listener
-      LINK_RE = %r(href=\"(.*?)\")
+      LINK_RE = %r(href=\\"(.*?)\\")
       reacts_to LINK_RE
       
       def process
